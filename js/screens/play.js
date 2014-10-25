6e241c1187dd1fd71a3dd0ea127210913138cefb
Jingyi Li <soundtracknoon@gmail.com>
2014-10-24 21:58:25 -0700

    spring is not gone

--- js/screens/play.js
+++ js/screens/play.js
@@ -4,7 +4,7 @@ game.PlayScreen = me.ScreenObject.extend({
 	 */
 	onResetEvent: function() {
 		//load level
-		me.levelDirector.loadLevel("winter");
+		me.levelDirector.loadLevel("spring");
 		//currently switch between area01, spring, summer, winter
 
 		// DEBUG IS HERE!!!!!!!!!!!!!!!!!!!!!
